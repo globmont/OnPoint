9024ae8fe741a3a1317e24daf223806784795cb4
Globmont <globmont@users.noreply.github.com>
2015-10-04 04:37:26 -0400

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,147 @@
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
+
+
+var UI = require('ui');
+var Vector2 = require('vector2');
+var Settings = require('settings');
+require('firebase');
+var Vibe = require('/ui/vibe');
+var Light = require('ui/light');
+
+
+var baseurl = "https://onpoint.firebaseio.com/";
+var key = keygen(5);
+var timers = [];
+
+
+function start(key) {
+  var shouldUpdateUI = true;
+  //key = "38A011750F21";
+  var slidesdb = new Firebase(baseurl + "shows/" + key);
+  var currentslidedb = new Firebase(baseurl + "monitor/" + key + "/currentSlide");
+  var monitordb = new Firebase(baseurl + "monitor/" + key);
+  var keydb = new Firebase(baseurl + "monitor/");
+  var offsetdb = new Firebase(baseurl + "monitor/" + key + "/offset");
+  var defaultMonitors = {index: Date.now(), currentSlide: -1, offset: 0};
+  monitordb.set(defaultMonitors);
+  
+  var slides;
+  
+  slidesdb.on('value', function(data) {
+    console.log("Got slides: " + JSON.stringify(data.val()));
+    slides = data.val();
+    currentslidedb.once('value', currentSlideChanged);
+  });
+  
+  var main = new UI.Card({
+    title: ' On Point',
+    icon: 'images/icon.png',
+    subtitle: 'Enter key in PowerPoint.',
+    body: 'Key: ' + key
+  });
+  
+  main.show();
+  
+  
+  
+  currentslidedb.on('value', currentSlideChanged);
+  function currentSlideChanged(snapshot) {
+    console.log("Child changed: " + snapshot.val());
+    for(i = 0; i < timers.length; i++) {
+      clearInterval(timers[i]);
+    }
+    if(snapshot.val() >= 0) {
+      var name = slides[snapshot.val()].name;
+      var time = slides[snapshot.val()].time;
+      console.log("Slide " + name + " " + time);
+      var seconds = time;
+      main.subtitle(name);
+      main.body(seconds + " secs left.");
+      
+      
+      var timer = function() {
+          if(shouldUpdateUI) {
+            main.subtitle(name);
+            main.body(seconds + " secs left.");
+          }
+          seconds--;
+          if(seconds < 0) {
+            clearInterval(t);
+            Light.trigger();
+            Vibe.vibrate('double');
+            Vibe.vibrate('long');
+          }
+      }
+      
+      
+      var t = setInterval(timer, 1000);    
+      timers.push(t);
+    } else if(snapshot.val() == -1) {
+      main.subtitle("Enter key in PowerPoint.");
+      main.body("Key: " + key);
+    }
+  }
+  
+  main.on('click', 'up', function(e) {
+    offsetdb.once('value', function(data) {
+      var offset = data.val() + 1;
+      monitordb.update({offset: offset});
+    });
+  });
+  
+  main.on('click', 'select', function(e) {
+    shouldUpdateUI = !shouldUpdateUI;
+    
+    keydb.orderByChild("index").once('value', function(data) {
+      var keys = [];
+      
+      
+      data.forEach(function(snap) {
+        keys.unshift({title: snap.key()});
+      })
+      var menu = new UI.Menu({
+        sections: [{
+          title: 'Past Keys',
+          items: keys
+        }]
+      });
+      
+      
+      menu.on('select', function(e) {
+        selecteddb = new Firebase(baseurl + "monitor/" + e.item.title)
+        key = e.item.title;
+        selecteddb.update({
+          index: Date.now()
+        });
+        main.hide();
+        menu.hide();
+        start(key);
+      });
+      
+      menu.show();
+    });
+    
+    
+    
+  });
+  
+  main.on('click', 'down', function(e) {
+    offsetdb.once('value', function(data) {
+      var offset = data.val() - 1;
+      monitordb.update({offset: offset});
+    });
+  });
+  
+}  
+  
+  
+  function keygen(length) {
+    return Math.random().toString(36).substring(2, 2 + length);
+  }
+
+
+start(key);
